95d6df09d053faa5a6896fa1a8448c514cd16672
Jonathan <jonathan_plata@ymail.com>
2018-11-14 10:13:05 -0800

    functions

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var KEY = process.env.KEY;
+const KEY = process.env.KEY;
+const FILE_HEAD = process.argv[2];
+const FILE = FILE_HEAD + '.csv';
 
 if(KEY === undefined) {
 	throw new Error('need api key');
 } 
 
+var fs = require('fs');
+var parse = require('csv-parse');
+var json2csv = require('json2csv').Parser;
 var googleMapsClient = require('@google/maps').createClient({
   key: KEY
 });
 
+
+function start() {
+
+	var json_file;
+
+	fs.createReadStream(FILE).pipe(parsse({
+		delimeter: ',',
+		columns: true
+	}, (err, data) => {
+
+		if(err) throw err;
+
+		json_file = data;
+		
+	}));
+
+	console.log(json_file);
+}
 
 function geoCodeArray(array) {
 
